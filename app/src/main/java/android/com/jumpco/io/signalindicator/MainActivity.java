cef93494bd6eea2f25aef75efbbfe501acd0ac95
odwa <odwa.menziwa@jumpco.io>
2022-02-25 15:50:40 +0200

    created Util class and a method to check internet connection

--- app/src/main/java/android/com/jumpco/io/signalindicator/MainActivity.java
+++ app/src/main/java/android/com/jumpco/io/signalindicator/MainActivity.java
@@ -2,13 +2,48 @@ package android.com.jumpco.io.signalindicator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        isNetworkAvailable(context);
+    }
+
+    // check internet
+    public boolean isNetworkAvailable(Context context){
+        boolean isConnected = false;
+
+        ConnectivityManager connectionManager = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo[] allNetworkInfo = connectionManager.getAllNetworkInfo();
+
+        for (NetworkInfo networkInfoList : allNetworkInfo) {
+            if(networkInfoList.getTypeName().equalsIgnoreCase("WIFI")){
+                isConnected = true;
+                Toast.makeText(context,"Network Available " +isConnected,Toast.LENGTH_SHORT).show();
+            }
+            else {
+                isConnected = false;
+                Toast.makeText(context,"No Network Available "+isConnected,Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+
+
+        return isConnected;
     }
 }
---
+++ app/src/main/java/android/com/jumpco/io/signalindicator/Util.java
@@ -0,0 +1,11 @@
+package android.com.jumpco.io.signalindicator;
+
+import android.content.Context;
+
+import java.sql.Connection;
+
+public class Util {
+
+
+
+}
